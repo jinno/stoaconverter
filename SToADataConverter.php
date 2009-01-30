026765f37784422cdb6cb7d450d976ac2de35900
jinno <sjinnoenter@gmail.com>
2009-01-30 22:00:36 +0900

    eventLog出力まで対応
    Signed-off-by: jinno <sjinnoenter@gmail.com>

--- SToADataConverter.php
+++ SToADataConverter.php
@@ -1,5 +1,9 @@
 <?php
-include_once(dirname(__FILE__).'/simulation_dat_conf.php');
+/* TODO
+とりあえずバラバラのループでデータを取得
+最終的に同じバッファループ内でデータ取得を切り替えられるように直す
+*/
+include_once(dirname(__FILE__).'/simulation_conf.php');
     /****************
      定数定義
     *****************/
@@ -79,6 +83,7 @@ include_once(dirname(__FILE__).'/simulation_dat_conf.php');
         /*******************************************
          simulation_datを書き出し
         ********************************************/
+writeLog('simulation_dat start!!!',$sequenceName);
         //書き出しファイル名の設定
         $fileName   = $directory.'/'.'simulation_'.CREATE_DATE.'_'.$sequenceName.'.dat';
 
@@ -91,30 +96,30 @@ include_once(dirname(__FILE__).'/simulation_dat_conf.php');
         for($iCt=0;$iCt < $bufferCt;$iCt++){
             if(($iCt < 200 && DEBUG) || !DEBUG){
                 if($buffer[$iCt] == $key){
-writeLog('【$key】:'.$key,$sequenceName);
+//writeLog('【$key】:'.$key,$sequenceName);
                     $downLow    = $dat_conf[$key]['downLow'];                           //データのある行を確認
-writeLog('【$downLow】:'.$downLow,$sequenceName);
+//writeLog('【$downLow】:'.$downLow,$sequenceName);
                     $isAnyFloor = isset($dat_conf[$key]['isAnyFloor']) ? true : false;  //階数分のデータがあるのか確認
 
-if($isAnyFloor) writeLog('【$isAnyFloor】:'.$isAnyFloor,$sequenceName);
+//if($isAnyFloor) writeLog('【$isAnyFloor】:'.$isAnyFloor,$sequenceName);
 
                     for($floorCt=0;$floorCt < $floorNum;$floorCt++){
                         if(!$isAnyFloor && $floorCt > 0) break;
                         $str = $buffer[$iCt + $downLow + $floorCt]; //データの取得
-writeLog('thisLoopBuffor:'.$str,$sequenceName);
+//writeLog('thisLoopBuffor:'.$str,$sequenceName);
                         $pat = '/('.$dat_conf[$key]['pattern'].')/';
-writeLog('$pat:'.$pat,$sequenceName);
+//writeLog('$pat:'.$pat,$sequenceName);
                         //$str = preg_replace($pat, \1, $str);
                         preg_match($pat, $str, $match);
                         array_shift($match);
                         $str = join(', ', $match);
-writeLog('replaced:'.$str,$sequenceName);
+//writeLog('replaced:'.$str,$sequenceName);
 
                         if($key == FLOOR_NUM_KEY){
                             $str = trim($str,'データ取得成功：');
                             $str = trim($str);
                             $floorNum = intVal($str);       //階数を取得
-writeLog('■$floorNum:'.$floorNum,$sequenceName);
+//writeLog('■$floorNum:'.$floorNum,$sequenceName);
                         }
                                                                     //データを加工する
                         $tmpData[] = $str;
@@ -138,24 +143,103 @@ writeLog('■$floorNum:'.$floorNum,$sequenceName);
                 break;
             }
         }
+writeLog('simulation_dat end!!!',$sequenceName);
 
         /*******************************************
          eventLogを書き出し
         ********************************************/
+writeLog('eventLog stert!!!',$sequenceName);
         $div = 1;
         //書き出しファイル名の設定
-        $fileName   = $directory.'/'.'eventlog_'.CREATE_DATE.'_'.$sequenceName.'_'.$div.'.csv';
+        $eventLogFileName[1]   = $directory.'/'.'eventlog_'.CREATE_DATE.'_'.$sequenceName.'_1.csv';
+        $eventLogFileName[2]   = $directory.'/'.'eventlog_'.CREATE_DATE.'_'.$sequenceName.'_2.csv';
+        $cagePersonsPast = 0;   //前回カゴ人数
 
-        $startStr   = '';
-        $endStr     = '';
+        $startStr       = 'Final Local Time';
+        $endStr         = 'EleLife SummarySimulation\s*\d\s*Start';
+        $startFinded    = false;
+        $headerWritn    = false;
 
+        //headerの設定
+        $header     = 'Now----, Next---, Cage, LocalTime--, Pos, hw---------, Dep, Arr, SD, Clt--------, Ton, Tof, Hc-';
+$mCt = 0;
         for($iCt=0;$iCt < $bufferCt;$iCt++){
-            if($buffer[$iCt] == $startStr) continue;
-//writeLog('【$key】:'.$key,$sequenceName);
-                $str = $buffer[$iCt + $downLow + $floorCt]; //データの取得
-                                                            //データを加工する
-                writeFile($fileName,$str);                  //ファイルに書き出し
+            if(preg_match('/'.$endStr.'/', $buffer[$iCt]) == 1){  //終了文字列行にマッチしたら
+                $startFinded = false;   //開始文字列行発見フラグをfalse
+                $headerWritn = false;   //ヘッダ行を書き出し完了フラグをFalse
+                $div++;
+writeLog('■■MATCH！■■',$sequenceName);
+writeLog('eventLog pat:/'.$endStr.'/',$sequenceName);
+writeLog("eventLog buffer[$iCt]:".$buffer[$iCt],$sequenceName);
+                continue;
+            }
+
+            if(preg_match('/'.$startStr.'/', $buffer[$iCt]) == 1) {
+                $startFinded = true;
+writeLog('■■MATCH！■■',$sequenceName);
+writeLog('eventLog pat:/'.$startStr.'/',$sequenceName);
+writeLog("eventLog buffer[$iCt]:".$buffer[$iCt],$sequenceName);
+                continue;
+            }
+            //開始文字列行にマッチしたら開始文字列行発見フラグをtrue
+            if($startFinded != true) continue;  //開始文字列行がfalseなら、コンティニュー
+            if($startFinded == true){           //開始文字列行がtrueなら
+                if($headerWritn == false){          //ヘッダ行を書き出し完了フラグがfalseなら
+                    $headerWritn    = true;             //ヘッダ行を書き出し完了フラグをtrue
+writeLog("eventLogFileName[$div]:".$eventLogFileName[$div],$sequenceName);
+                    writeFile($eventLogFileName[$div],$header); //headerの書き出し
+                }
+            }
+writeLog('【$key】:'.$key,$sequenceName);
+            $str = $buffer[$iCt]; //データの取得
+            //データを加工する
+            // matchでデータを取得
+            //Now, Next, Cage(div), LocalTime(clt), Pos, hw, Dep, Arr,  SD, Clt, Ton, Tof, Hc
+            //Now, Next,       itv,          Cycle, Pos, hw, Dep, Arr, Dir, Clt,           Hc
+            $eventPat = '/^[^\d]+(\d+\.[A-Z]{4})[^\d]+(\d+\.[A-Z]{4})[^\d]+([0-9\.]+)[^\d]+([0-9\.]+)[^\d]+([0-9\.]+)[^\d]+([0-9\.e\+\-]+)[^\d]+(\d+)[^\d]+(\d+)[^\d]*Dir=([A-Z\-]{2})[^\d]+([0-9\.e\+\-]+)[^\d]+(\d+)/';
+            preg_match($eventPat, $str, $match);
+writeLog('$eventPat:'.$eventPat,$sequenceName);
+writeLog('$str:'.$str,$sequenceName);
+            array_shift($match);
+            //乗降人数の算出(乗降のタイミングは必ずずれるのでどちらかのみ)
+            $ton = 0;//乗車
+            $tof = 0;//降車
+            $cagePersonsDiff = $match[10] - $cagePersonsPast;
+            if ($cagePersonsDiff > 0) { // 前回より増えたら
+                $ton = $cagePersonsDiff;    //tonに設定
+            } else
+            if ($cagePersonsDiff < 0) { // 減ったら
+                $tof = abs($cagePersonsDiff);    //tofに設定(マイナスなので絶対値にする)
+            }
+            $cagePersonsPast = $match[10];  // 今回の値を保存
+            
+            // 並びかえる
+            if(isset($sortedEventData)) unset($sortedEventData);
+            $sortedEventData[0]     = $match[0];                                //Now
+            $sortedEventData[1]     = $match[1];                                //Next
+            $sortedEventData[2]     = str_pad($div, 4, ' ' ,STR_PAD_LEFT);      //Cage(div)
+            $sortedEventData[3]     = $match[9];                                //LocalTime(Clt)
+            $sortedEventData[4]     = str_pad($match[4], 3, ' ' ,STR_PAD_LEFT); //Pos
+            $sortedEventData[5]     = $match[5];                                //hw
+            $sortedEventData[6]     = str_pad($match[6], 3, ' ' ,STR_PAD_LEFT); //Dep
+            $sortedEventData[7]     = str_pad($match[7], 3, ' ' ,STR_PAD_LEFT); //Arr
+            $sortedEventData[8]     = $match[8];                                //SD
+            $sortedEventData[9]     = $match[9];                                //Clt
+            $sortedEventData[10]    = str_pad($ton, 3, ' ' ,STR_PAD_LEFT);      //Ton
+            $sortedEventData[11]    = str_pad($tof, 3, ' ' ,STR_PAD_LEFT);      //Tof
+            $sortedEventData[12]    = str_pad($match[10], 3, ' ' ,STR_PAD_LEFT);//Hc
+            // カンマで連結
+            $str = join(', ', $sortedEventData);
+writeLog('sortedEventData（'.$iCt.'）:'.$str,$sequenceName);
+
+//if($mCt > 10) {
+//    break;
+//}
+//$mCt++;
+
+            writeFile($eventLogFileName[$div],$str);                  //ファイルに書き出し
         }
+writeLog('eventLog end!!!',$sequenceName);
 
         /*****************
          footer読み込み
